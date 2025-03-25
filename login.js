5643da7ddba8ff0bf6dcd4edf042c8fdbab7e5b3
mert138 <82927730+mert138@users.noreply.github.com>
2025-03-25 14:58:33 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
 // Firebase yapılandırması
 const firebaseConfig = {
-    apiKey: "AIzaSyCEu0jR_Pdru5ry7Gv8I_61ruajssZVm0k",
-    authDomain: "tremila-shop-133ce.firebaseapp.com",
-    projectId: "tremila-shop-133ce",
-    storageBucket: "tremila-shop-133ce.firebasestorage.app",
-    messagingSenderId: "249334077736",
-    appId: "1:249334077736:web:4f9cb53aec4bbf3d17181d",
-    measurementId: "G-Q8VN1F66LW"
+  apiKey: "AIzaSyCEu0jR_Pdru5ry7Gv8I_61ruajssZVm0k",
+  authDomain: "tremila-shop-133ce.firebaseapp.com",
+  projectId: "tremila-shop-133ce",
+  storageBucket: "tremila-shop-133ce.firebasestorage.app",
+  messagingSenderId: "249334077736",
+  appId: "1:249334077736:web:4f9cb53aec4bbf3d17181d",
+  measurementId: "G-Q8VN1F66LW
 };
 
 // Firebase'i başlat
